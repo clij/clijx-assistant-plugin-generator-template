d7f0554c8b5f9cb936f80a535cc50badb0df635c
haesleinhuepf <rhaase@mpi-cbg.de>
2020-09-14 10:19:47 +0200

    initial version

---
+++ src/main/java/net/haesleinhuepf/clijx/EnterClassNameHere.java
@@ -0,0 +1,12 @@
+package net.haesleinhuepf.clijx;
+
+/**
+ *
+ * Author: haesleinhuepf
+ *         enter_date_here
+ */
+public class EnterClassNameHere {
+    public static void main(String[] args) {
+
+    }
+}
